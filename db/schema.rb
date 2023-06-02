72de531036530965a415c23a08b7ebc588228a4f
neweos <65535758+neweos@users.noreply.github.com>
2023-06-02 21:04:12 +0800

    neweos created a commit :sparkles:

--- db/schema.rb
+++ db/schema.rb
@@ -12,83 +12,83 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_06_02_093659) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "citext"
-  enable_extension "plpgsql"
+  enable_extension 'citext'
+  enable_extension 'plpgsql'
 
-  create_table "account_login_change_keys", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "login", null: false
-    t.datetime "deadline", null: false
+  create_table 'account_login_change_keys', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'login', null: false
+    t.datetime 'deadline', null: false
   end
 
-  create_table "account_password_reset_keys", force: :cascade do |t|
-    t.string "key", null: false
-    t.datetime "deadline", null: false
-    t.datetime "email_last_sent", default: -> { "CURRENT_TIMESTAMP" }, null: false
+  create_table 'account_password_reset_keys', force: :cascade do |t|
+    t.string 'key', null: false
+    t.datetime 'deadline', null: false
+    t.datetime 'email_last_sent', default: -> { 'CURRENT_TIMESTAMP' }, null: false
   end
 
-  create_table "account_remember_keys", force: :cascade do |t|
-    t.string "key", null: false
-    t.datetime "deadline", null: false
+  create_table 'account_remember_keys', force: :cascade do |t|
+    t.string 'key', null: false
+    t.datetime 'deadline', null: false
   end
 
-  create_table "account_verification_keys", force: :cascade do |t|
-    t.string "key", null: false
-    t.datetime "requested_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
-    t.datetime "email_last_sent", default: -> { "CURRENT_TIMESTAMP" }, null: false
+  create_table 'account_verification_keys', force: :cascade do |t|
+    t.string 'key', null: false
+    t.datetime 'requested_at', default: -> { 'CURRENT_TIMESTAMP' }, null: false
+    t.datetime 'email_last_sent', default: -> { 'CURRENT_TIMESTAMP' }, null: false
   end
 
-  create_table "accounts", force: :cascade do |t|
-    t.integer "status", default: 1, null: false
-    t.citext "email", null: false
-    t.string "password_hash"
-    t.index ["email"], name: "index_accounts_on_email", unique: true, where: "(status = ANY (ARRAY[1, 2]))"
+  create_table 'accounts', force: :cascade do |t|
+    t.integer 'status', default: 1, null: false
+    t.citext 'email', null: false
+    t.string 'password_hash'
+    t.index ['email'], name: 'index_accounts_on_email', unique: true, where: '(status = ANY (ARRAY[1, 2]))'
   end
 
-  create_table "audits", force: :cascade do |t|
-    t.integer "auditable_id"
-    t.string "auditable_type"
-    t.integer "associated_id"
-    t.string "associated_type"
-    t.integer "user_id"
-    t.string "user_type"
-    t.string "username"
-    t.string "action"
-    t.text "audited_changes"
-    t.integer "version", default: 0
-    t.string "comment"
-    t.string "remote_address"
-    t.string "request_uuid"
-    t.datetime "created_at"
-    t.index ["associated_type", "associated_id"], name: "associated_index"
-    t.index ["auditable_type", "auditable_id", "version"], name: "auditable_index"
-    t.index ["created_at"], name: "index_audits_on_created_at"
-    t.index ["request_uuid"], name: "index_audits_on_request_uuid"
-    t.index ["user_id", "user_type"], name: "user_index"
+  create_table 'audits', force: :cascade do |t|
+    t.integer 'auditable_id'
+    t.string 'auditable_type'
+    t.integer 'associated_id'
+    t.string 'associated_type'
+    t.integer 'user_id'
+    t.string 'user_type'
+    t.string 'username'
+    t.string 'action'
+    t.text 'audited_changes'
+    t.integer 'version', default: 0
+    t.string 'comment'
+    t.string 'remote_address'
+    t.string 'request_uuid'
+    t.datetime 'created_at'
+    t.index ['associated_type', 'associated_id'], name: 'associated_index'
+    t.index ['auditable_type', 'auditable_id', 'version'], name: 'auditable_index'
+    t.index ['created_at'], name: 'index_audits_on_created_at'
+    t.index ['request_uuid'], name: 'index_audits_on_request_uuid'
+    t.index ['user_id', 'user_type'], name: 'user_index'
   end
 
-  create_table "gutentag_taggings", force: :cascade do |t|
-    t.integer "tag_id", null: false
-    t.integer "taggable_id", null: false
-    t.string "taggable_type", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["tag_id"], name: "index_gutentag_taggings_on_tag_id"
-    t.index ["taggable_type", "taggable_id", "tag_id"], name: "unique_taggings", unique: true
-    t.index ["taggable_type", "taggable_id"], name: "index_gutentag_taggings_on_taggable_type_and_taggable_id"
+  create_table 'gutentag_taggings', force: :cascade do |t|
+    t.integer 'tag_id', null: false
+    t.integer 'taggable_id', null: false
+    t.string 'taggable_type', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['tag_id'], name: 'index_gutentag_taggings_on_tag_id'
+    t.index ['taggable_type', 'taggable_id', 'tag_id'], name: 'unique_taggings', unique: true
+    t.index ['taggable_type', 'taggable_id'], name: 'index_gutentag_taggings_on_taggable_type_and_taggable_id'
   end
 
-  create_table "gutentag_tags", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "taggings_count", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_gutentag_tags_on_name", unique: true
-    t.index ["taggings_count"], name: "index_gutentag_tags_on_taggings_count"
+  create_table 'gutentag_tags', force: :cascade do |t|
+    t.string 'name', null: false
+    t.integer 'taggings_count', default: 0, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['name'], name: 'index_gutentag_tags_on_name', unique: true
+    t.index ['taggings_count'], name: 'index_gutentag_tags_on_taggings_count'
   end
 
-  add_foreign_key "account_login_change_keys", "accounts", column: "id"
-  add_foreign_key "account_password_reset_keys", "accounts", column: "id"
-  add_foreign_key "account_remember_keys", "accounts", column: "id"
-  add_foreign_key "account_verification_keys", "accounts", column: "id"
+  add_foreign_key 'account_login_change_keys', 'accounts', column: 'id'
+  add_foreign_key 'account_password_reset_keys', 'accounts', column: 'id'
+  add_foreign_key 'account_remember_keys', 'accounts', column: 'id'
+  add_foreign_key 'account_verification_keys', 'accounts', column: 'id'
 end
