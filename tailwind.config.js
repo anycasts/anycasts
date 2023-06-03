d0053cab47ffee519cdc9676a53a62c38aa778eb
neweos <65535758+neweos@users.noreply.github.com>
2023-06-03 16:41:48 +0800

    neweos created a commit :sparkles:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.{js,ts,tsx}',
+    './app/components/**/*.html.erb',
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb'
+  ],
   theme: {
     extend: {}
   },
